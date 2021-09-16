7e19dee43ffe02ad1bc4606efe84bfa7dde676d9
hotcoffeehero <hotcoffeehero@gmail.com>
2021-09-16 12:51:59 +0900

    Schwartzmuller React Refresher II

--- React_Projects/webmini_bonus/src/components/header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Header = () => {
-    return <div>The date is...</div>
-}
-
-export default Header
--- schwartzmuller/src/App.js
+++ schwartzmuller/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import GoalList from './components/GoalList'
+import GoalList from './components/GoalList/GoalList'
+import NewGoal from './components/NewGoal/NewGoal'
 import './App.css'
 
 const App = () => {
+  const courseGoals = [
+    { id: 'cg1', text: 'Finish the course' },
+    { id: 'cg2', text: 'Learn all the things about React' },
+    { id: 'cg3', text: 'Help other students with stuff' },
+  ]
+
+  const addNewGoalHandler = (newGoal) => {
+    courseGoals.push(newGoal)
+    console.log(courseGoals)
+  }
+
   return (
     <div className='course-goals'>
       <h2>Course Goals</h2>
-      <GoalList />
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
     </div>
   )
 }
