1fea7d0972e857a85943fce3daac57b7bada8447
hotcoffeehero <hotcoffeehero@gmail.com>
2021-09-18 21:23:55 +0900

    React Refresher 4

--- schwartzmuller/src/components/NewGoal/NewGoal.js
+++ schwartzmuller/src/components/NewGoal/NewGoal.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NewGoal.css'
 
 const NewGoal = (props) => {
+  const [enteredText, setEnteredText] = useState('')
+
   const addGoalHandler = (e) => {
     e.preventDefault()
 
     const newGoal = {
       id: Math.random().toString(),
-      text: 'Deploy to Hotcoffee HQ',
+      text: enteredText,
     }
+
+    setEnteredText('')
 
     props.onAddGoal(newGoal)
   }
 
+  const textChangeHandler = (e) => {
+    setEnteredText(e.target.value)
+  }
+
   return (
     <form className='new-goal' onSubmit={addGoalHandler}>
-      <input type='text' />
+      <input type='text' value={enteredText} onChange={textChangeHandler} />
       <button type='submit'>Add Goal</button>
     </form>
   )
