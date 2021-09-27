766d57032e7ee1334162e989ee4dcf4c22a6aa54
hotcoffeehero <hotcoffeehero@gmail.com>
2021-09-27 15:33:42 +0900

    52: Adding a Modal

--- sightshare/src/shared/components/UIElements/Modal.js
+++ sightshare/src/shared/components/UIElements/Modal.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-router-dom'
+import ReactDOM from 'react-dom'
+import Backdrop from './Backdrop'
+import { CSSTransition } from 'react-transition-group'
 
 import './Modal.css'
 
@@ -11,10 +13,15 @@ const ModalOverlay = (props) => {
       </header>
       <form
         onSubmit={
-          props.onSubmit ? props.onSubmit : (event) => event.preventDefault
+          props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
         }
       >
-        <div className={`modal__content ${props.contentClass}`}></div>
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
+        </div>
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
       </form>
     </div>
   )
@@ -22,7 +29,20 @@ const ModalOverlay = (props) => {
 }
 
 const Modal = (props) => {
-  return <div></div>
+  return (
+    <React.Fragment>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames='modal'
+      >
+        <ModalOverlay {...props} />
+      </CSSTransition>
+    </React.Fragment>
+  )
 }
 
 export default Modal
