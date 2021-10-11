d423445fa75c839c84be5713ff60f613674d3a91
hotcoffeehero <hotcoffeehero@gmail.com>
2021-10-11 15:45:06 +0900

    89: place-specific routes

--- sightshare/backend/app.js
+++ sightshare/backend/app.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser')
 const placeRoutes = require('./routes/place-routes')
 const app = express()
 
-app.use(placeRoutes)
+app.use('/api/places', placeRoutes)
+
 app.listen(5000, () => {
   console.log('Go on 5000')
 })
--- sightshare/backend/routes/place-routes.js
+++ sightshare/backend/routes/place-routes.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-  console.log('Get Request Gotten, lol')
-  res.json({ message: "It's working." })
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'The coolest place in New York City',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: '20 W 34th St, New York, NY, 10001',
+    creator: 'u1',
+  },
+]
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId
+  })
+  res.json({ place })
 })
 
 module.exports = router
