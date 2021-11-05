2d672a93b7d7e1c7cf2961c1093df14da288b3fe
hotcoffeehero <hotcoffeehero@gmail.com>
2021-11-05 12:05:16 +0900

    94: Adding Controllers

--- sightshare/backend/routes/place-routes.js
+++ sightshare/backend/routes/place-routes.js
@@ -3,45 +3,11 @@ const router = express.Router()
 
 const HttpError = require('../models/http-error')
 
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'The coolest place in New York City',
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: '20 W 34th St, New York, NY, 10001',
-    creator: 'u1',
-  },
-]
+const placeControllers = require('../controllers/places-controllers')
 
-router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId
-  })
 
-  if (!place) {
-    throw new HttpError("...couldn't find what you were looking for.", 404)
-    
-  }
+router.get('/:pid', placeControllers.getPlaceById)
 
-  res.json({ place })
-})
-
-router.get('/user/:uid', (req, res, next) => {
-  const userId = req.params.uid
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId
-  })
-
-  if (!place) {
-    
-    return next(new HttpError("...couldn't find a place for that id."))
-  }
-  res.json({ place })
-})
+router.get('/user/:uid', placeControllers.getPlaceByUserId)
 
 module.exports = router
