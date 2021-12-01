1df30c2ce3c893260915c334ea3fe182495ee2ba
hotcoffeehero <hotcoffeehero@gmail.com>
2021-12-02 02:51:34 +0900

    Sightshare, attempt 2, commit 1, setup

--- sightshare/frontend/src/App.js
+++ sightshare/frontend/src/App.js
@@ -1,81 +1,26 @@
-import React, { useState, useCallback } from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch,
-} from 'react-router-dom'
-
+import React from 'react'
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
-import UserPlaces from './places/pages/UserPlaces'
-import UpdatePlace from './places/pages/UpdatePlace'
-import MainNavigation from './shared/components/Navigation/MainNavigation'
-import Auth from './user/pages/Auth'
-import { AuthContext } from './shared/context/auth-context'
 
-const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+function App() {
+  return <Router>
 
-  const login = useCallback(() => {
-    setIsLoggedIn(true)
-  }, [])
+    <Switch>
 
-  const logout = useCallback(() => {
-    setIsLoggedIn(false)
-  }, [])
+    <Route path="/" exact>
+      <Users />
+    </Route>
 
-  let routes
+    <Route path="/places/new" exact>
+      <NewPlace />
+    </Route>
 
-  if (isLoggedIn) {
-    routes = (
-      <Switch>
-        <Route path='/' exact>
-          <Users />
-        </Route>
-        <Route path='/:userId/places' exact>
-          <UserPlaces />
-        </Route>
-        <Route path='/places/new' exact>
-          <NewPlace />
-        </Route>
-        <Route path='/places/:placeId'>
-          <UpdatePlace />
-        </Route>
-        <Redirect to='/' />
-      </Switch>
-    )
-  } else {
-    routes = (
-      <Switch>
-        <Route path='/' exact>
-          <Users />
-        </Route>
-        <Route path='/:userId/places' exact>
-          <UserPlaces />
-        </Route>
-        <Route path='/auth'>
-          <Auth />
-        </Route>
-        <Redirect to='/auth' />
-      </Switch>
-    )
-  }
+    <Redirect to="/" />
 
-  return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn: isLoggedIn,
-        login: login,
-        logout: logout,
-      }}
-    >
-      <Router>
-        <MainNavigation />
-        <main>{routes}</main>
-      </Router>
-    </AuthContext.Provider>
-  )
+    </Switch>
+
+  </Router>
 }
 
 export default App
